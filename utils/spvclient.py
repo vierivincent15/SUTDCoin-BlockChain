27fc9b6e87ed52a807a7a5e262b05968e584a3a7
vierivincent15 <vieri.vincent15@gmail.com>
2020-03-17 23:29:41 +0800

    client able to accept header

--- utils/spvclient.py
+++ utils/spvclient.py
@@ -2,10 +2,12 @@ try:
     from transaction import Transaction
     from blockchain import Blockchain
     from block import Block
+    from merkletree import verify_proof
 except:
     from utils.transaction import Transaction
     from utils.blockchain import Blockchain
     from utils.block import Block
+    from utils.merkletree import verify_proof
 from ecdsa import SigningKey
 import hashlib
 import time
@@ -22,6 +24,7 @@ class SPVClient(object):
         newClient.block_headers = []
         newClient.id = client_id
         newClient.balance = balance
+        newClient.headers = [[]]
         return newClient
 
     def receive_block_header(self, blockchain):
@@ -42,6 +45,42 @@ class SPVClient(object):
 
     def verify_transaction(self, transaction):
         return transaction.validate()
+
+    def add_header(self, header):
+        idxs = self.trace_prev_header(header["prev_header"])
+        hc_idx, h_idx = 0, 0
+        if idxs != -1:
+            hc_idx, h_idx = idxs
+        if (h_idx == len(self.headers[hc_idx]) - 1) or h_idx == 0:
+            self.headers[hc_idx].append(header)
+        else:
+            temp_header = self.headers[hc_idx][:h_idx+1].copy()
+            temp_header.append(header)
+            self.headers.append(temp_header)
+
+
+    def trace_prev_header(self, prev_header):
+        for i in range(len(self.headers)):
+            for j in range(len(self.headers[i])):
+                if self.headers[i][j] == prev_header:
+                    return (i, j)
+
+        # meaning prev_header not found in chain
+        return -1
+
+    def identify_longest_header(self):
+        chain_length = [len(chain) for chain in self.headers]
+        max_index = chain_length.index(max(chain_length))
+
+        return max_index
+
+    def validate_transaction(self, transaction, proof, tree_root):
+        longest_idx = self.identify_longest_header()
+        merkle_roots = [header["tree_root"] for header in self.headers[longest_idx]]
+        if tree_root not in merkle_roots:
+            return False
+        return verify_proof(transaction, proof, tree_root)
+        
 
 
 if __name__ == "__main__":
