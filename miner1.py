3b956cbb43a43c5cb440a627efe656528e113a6a
edricwu <edricwu008@gmail.com>
2020-03-23 16:30:30 +0800

    fix print

--- miner1.py
+++ miner1.py
@@ -97,8 +97,8 @@ def receive_block():
     json_block = request.form['block']
     block = Block.deserialize(json_block)
     blockchain.add_block(block)
-    x = [len(chain) for chain in blockchain.blockchains]
-    print(x)
+    chain_len = [len(chain) for chain in blockchain.blockchains]
+    print(chain_len)
     for tid in pending_tx.keys():
         pending_tx[tid] = pending_tx[tid] - 1
 
